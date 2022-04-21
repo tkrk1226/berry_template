8a88e3de0712706c5ee3c53c88213915c37f2272
UijinJung <tkrk1226@ajou.ac.kr>
2022-04-21 21:38:34 +0900

    berry 0421

---
+++ src/views/dashboard/Default/index.js
@@ -0,0 +1,18 @@
+// material-ui
+import { Grid } from '@mui/material';
+
+// project imports
+import { gridSpacing } from 'store/constant';
+
+// ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const Dashboard = () => {
+
+    return (
+        <Grid container spacing={gridSpacing}>
+
+        </Grid>
+    );
+};
+
+export default Dashboard;
